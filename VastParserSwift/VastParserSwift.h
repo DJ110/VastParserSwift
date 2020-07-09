f07f33e95afbad20e7c4ae4f6541ee948bcb7520
DJ110 <atmarkplant@gmail.com>
2020-07-09 21:47:49 +0800

    Initial Commit

---
+++ VastParserSwift/VastParserSwift.h
@@ -0,0 +1,19 @@
+//
+//  VastParserSwift.h
+//  VastParserSwift
+//
+//  Created by 伊藤大司 on 7/9/20.
+//  Copyright © 2020 DJ110. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VastParserSwift.
+FOUNDATION_EXPORT double VastParserSwiftVersionNumber;
+
+//! Project version string for VastParserSwift.
+FOUNDATION_EXPORT const unsigned char VastParserSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VastParserSwift/PublicHeader.h>
+
+
